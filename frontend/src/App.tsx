ee02b5b22426de374667563ee68365d27a4703ad
atneon27 <aryan.tiwari.at27@gmail.com>
2025-05-09 18:20:28 +0530

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-dvh bg-black text-white text-3xl font-semibold">
+      Hello
+    </div>
+  )
+}
+
+export default App; 
